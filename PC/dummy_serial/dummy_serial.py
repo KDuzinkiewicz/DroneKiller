c1c255fc912b9abb047be36cfc5f97314549171f
Karol J. Duzinkiewicz <karol.duzinkiewicz@gmail.com>
2022-03-12 19:21:03 +0100

    Added basic logging functionality

--- PC/dummy_serial/dummy_serial.py
+++ PC/dummy_serial/dummy_serial.py
@@ -1,5 +1,23 @@
 import cv2
 import numpy as np
 import serial
+import logging
 
-print('Karol jest super')
+
+ARDUINO_SERIAL_PORT = 'COM7'
+
+# set root logger log level
+logging.getLogger().setLevel(logging.INFO)
+
+def main():
+    logging.info('*** Serial Port Example ***')
+    logging.info('Opening serial port...')
+    # create a port (it will be atomatically opened upon creation)
+    ser = serial.Serial(ARDUINO_SERIAL_PORT)
+    if not ser.is_open:
+        logging.error('Serial port not open.')
+    logging.info('Serial port open.')
+
+
+if __name__ == "__main__":
+    main()

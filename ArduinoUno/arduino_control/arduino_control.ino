cd299dc880d4bc7e957a40bfc7ea49a37f2921a4
Karol J. Duzinkiewicz <karol.duzinkiewicz@gmail.com>
2022-03-31 19:18:22 +0200

    Added command parsing

--- ArduinoUno/arduino_control/arduino_control.ino
+++ ArduinoUno/arduino_control/arduino_control.ino
@@ -11,6 +11,52 @@ Adafruit_DCMotor *linearActuator2 = AFMS.getMotor(2); // 2nd linear actuator for
 Adafruit_DCMotor *motorX = AFMS.getMotor(3); // DC motor for moving in the X axis
 Adafruit_DCMotor *motorY = AFMS.getMotor(4); // DC motor for moving in the Y axis
 
+// command received over serial port
+String command = "";
+
+
+void parse_and_execute_command(String command) {
+
+    if (command == "TRG") {
+        Serial.println("FIRE!!!");
+        // pull_trigger();
+    }
+    else if (command == "GUN_ON") {
+        Serial.println("Turn on gun motor");
+        gun_on();
+    }
+    else if (command == "GUN_OFF") {
+        Serial.println("Turn off gun motor");
+        gun_off();
+    }
+    else {
+        String tmp = command.substring(0, command.indexOf("_"));
+
+        if (tmp == "DC") {
+            Serial.println("Motor control");
+
+            String tmp2 = command.substring(command.indexOf("_") + 1);
+            String motor_speed_x_str = tmp2.substring(0, tmp2.indexOf("_"));
+            String motor_speed_y_str = tmp2.substring(tmp2.indexOf("_") + 1);
+
+            Serial.println(motor_speed_x_str);
+            Serial.println(motor_speed_y_str);
+
+            // motor control command
+            int motor_speed_x = motor_speed_x_str.toInt();
+            int motor_speed_y = motor_speed_y_str.toInt();
+        }
+        else {
+            Serial.println("CMD unknown");
+        }
+
+        // other commands are not supported
+    }
+
+    return;
+}
+
+
 void pull_trigger() {
     // turn on both linear actuators for a brief time to pull the trigger
     // NOTE: Single linear actuator can draw up to 4A during startup so we need to make it quick
@@ -24,33 +70,43 @@ void pull_trigger() {
 }
 
 
+void gun_on() {
+    // TODO: add code here
+}
+
+
+void gun_off() {
+    // TODO: add code here
+}
+
+
 void setup() {
     // set LED pin
     pinMode(13, OUTPUT);
 
-    // configure DC motors
-    if (AFMS.begin()) {
-        // motor shield found
+    // // configure DC motors
+    // if (AFMS.begin()) {
+    //     // motor shield found
 
-        //set the speed to start, from 0 (off) to 255 (max speed)
-        linearActuator1->setSpeed(255);
-        linearActuator1->run(FORWARD);
-        linearActuator2->setSpeed(255);
-        linearActuator2->run(FORWARD);
-        motorX->setSpeed(255);
-        motorX->run(FORWARD);
-        motorY->setSpeed(255);
-        motorY->run(FORWARD);
+    //     //set the speed to start, from 0 (off) to 255 (max speed)
+    //     linearActuator1->setSpeed(255);
+    //     linearActuator1->run(FORWARD);
+    //     linearActuator2->setSpeed(255);
+    //     linearActuator2->run(FORWARD);
+    //     motorX->setSpeed(255);
+    //     motorX->run(FORWARD);
+    //     motorY->setSpeed(255);
+    //     motorY->run(FORWARD);
 
-        // turn on all motors
-        linearActuator1->run(RELEASE);
-        linearActuator2->run(RELEASE);
-        motorX->run(RELEASE);
-        motorY->run(RELEASE);
-    }
-    else {
-        return;
-    }
+    //     // turn on all motors
+    //     linearActuator1->run(RELEASE);
+    //     linearActuator2->run(RELEASE);
+    //     motorX->run(RELEASE);
+    //     motorY->run(RELEASE);
+    // }
+    // else {
+    //     return;
+    // }
 
     // configure serial port
     // set up Serial library at 9600 bps
@@ -58,17 +114,28 @@ void setup() {
 }
 
 void loop() {
-    // pull the trigger
-    pull_trigger();
 
-    // let the motor controller to cool off
-    delay(5000);
+    if (Serial.available()) {
 
-    // blink LED to indicate that we are not in reset state
-    for (int i=0; i<3; i++) {
-      digitalWrite(13, HIGH);
-      delay(500);
-      digitalWrite(13, LOW);
-      delay(500);
+        // read a single character from serial port
+        char c = Serial.read();
+
+        if (c == '\n') {
+            Serial.println("CMD received");
+            Serial.println(command);
+            parse_and_execute_command(command);
+            command = "";
+
+            // signal receiving of the command
+            digitalWrite(13, HIGH);
+            delay(500);
+            digitalWrite(13, LOW);
+            delay(500);
+        }
+        else {
+            command += c;
+        }
     }
+
+    // TODO: Support other commands
 }
